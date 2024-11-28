0af3f4b2df56679884f3f05391661301214e5cc9
Qpm0 <110771708+Qpm0@users.noreply.github.com>
2024-11-28 20:17:32 +0200

    Add files via upload

---
+++ get_next_line/get_next_line.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpascu <rpascu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 01:11:50 by rpascu           #+#    #+#             */
+/*   Updated: 2023/01/18 21:26:36 by rpascu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*get_first_and_remaining_lines(char *str, char **remaining)
+{
+	size_t	i;
+	char	*first;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while ((str)[i] != '\n' && (str)[i])
+		i++;
+	first = ft_substr(str, 0, i + 1);
+	if ((str)[i] == '\n' && (str)[i + 1])
+		*remaining = ft_substr(str, i + 1, ft_strlen(str) - i - 1);
+	else
+		*remaining = NULL;
+	free(str);
+	return (first);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*buffer;
+	static char	*cache[1025];
+	size_t		n;
+
+	if (BUFFER_SIZE <= 0 || fd < 0 || 1025 < fd)
+		return (NULL);
+	else if (read(fd, 0, 0) < 0) // REMOVE this read, as new moulinette version gives invalid buffer_size read error
+	{
+		if (cache[fd])
+			free(cache[fd]);
+		return (cache[fd] = NULL, NULL);
+	}
+	buffer = malloc(sizeof(char) * BUFFER_SIZE + 1);
+	if (!buffer)
+		return (NULL);
+	n = 1;
+	while ((ft_strchr(cache[fd], '\n') == NULL) && n != 0)
+	{
+		n = read(fd, buffer, BUFFER_SIZE);
+		if (n <= 0)
+			break ;
+		buffer[n] = '\0';
+		cache[fd] = ft_strjoin(cache[fd], buffer);
+	}
+	return (free(buffer), get_first_and_remaining_lines(cache[fd], &cache[fd]));
+}
+/*
+#include <fcntl.h>
+#include <stdio.h>
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	int	i;
+
+	//int fd = 0;
+	fd = open("test.txt", O_RDONLY);
+	i = 0;
+	while (i++ < 5)
+	{
+	line = get_next_line(fd);
+	printf("%s", line);
+	free(line);
+	//sleep(10);
+	}
+}
+*/
---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rpascu <rpascu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/30 01:11:05 by rpascu           #+#    #+#             */
+/*   Updated: 2022/10/30 13:49:41 by rpascu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 100
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+char	*ft_strjoin(char *s1, const char *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
